108f986de8974df79a540db58bc042435d58ed29
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-02-10 12:58:15 +0430

    text input done

--- app/(auth)/(drawer)/(chat)/new.tsx
+++ app/(auth)/(drawer)/(chat)/new.tsx
@@ -1,19 +1,133 @@
-import { View, Text, Button } from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  Button,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
+import React, { useState } from 'react';
 import { useAuth } from '@clerk/clerk-react';
 import { StatusBar } from 'expo-status-bar';
+import { defaultStyles } from '@/constants/Styles';
+import { Stack } from 'expo-router';
+import HeaderDropDown from '@/components/HeaderDropDown';
+import MessageInput from '@/components/MessageInput';
 
-const index = () => {
+const Page = () => {
   const { signOut } = useAuth();
+  const [gptVersion, setGptVersion] = useState('3.5');
+
+  const getCompletion = (message: string) => {
+    console.log('getting completion for: ', message);
+  };
 
   return (
-    <View>
-      <StatusBar style='dark' />
+    <View style={defaultStyles.pageContainer}>
+      <Stack.Screen
+        options={
+          {
+            // headerTitle: () => (
+            //   <HeaderDropDown
+            //     title='ChatGPT'
+            //     onSelect={(key) => {
+            //       setGptVersion(key);
+            //     }}
+            //     selected={gptVersion}
+            //     items={[
+            //       { key: '3.5', title: 'GPT-3.5', icon: 'bolt' },
+            //       { key: '4', title: 'GPT-4', icon: 'sparkles' },
+            //     ]}
+            //   />
+            // ),
+          }
+        }
+      />
 
-      <Text>inside chat (chat)</Text>
-      <Button title='Sign out' onPress={() => signOut()} />
+      <ScrollView style={{ flex: 1 }}>
+        <Text>Dummy Content</Text>
+        {/* <Button title='Sign out' onPress={() => signOut()} /> */}
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+        <Text>Dummy Content</Text>
+      </ScrollView>
+      <KeyboardAvoidingView
+        keyboardVerticalOffset={70}
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          width: '100%',
+        }}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <MessageInput onShouldSendMessage={getCompletion} />
+      </KeyboardAvoidingView>
     </View>
   );
 };
 
-export default index;
+export default Page;
---
+++ components/MessageInput.tsx
@@ -0,0 +1,292 @@
+import Colors from '@/constants/Colors';
+import { FontAwesome6, Ionicons } from '@expo/vector-icons';
+import { View, StyleSheet } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import Animated, {
+  Extrapolation,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { useRef, useState } from 'react';
+import { BlurView } from 'expo-blur';
+import * as DocumentPicker from 'expo-document-picker';
+import * as ImagePicker from 'expo-image-picker';
+
+import { Text } from 'react-native';
+import React from 'react';
+
+const ATouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity);
+
+export type MessageInputProps = {
+  // TODO:
+  onShouldSendMessage: (message: string) => void;
+};
+
+const MessageInput = ({ onShouldSendMessage }: MessageInputProps) => {
+  const [message, setMessage] = useState('');
+  const { bottom } = useSafeAreaInsets();
+  const expanded = useSharedValue(0);
+
+  const expandItems = () => {
+    expanded.value = withTiming(1, { duration: 400 });
+  };
+
+  const collapseItems = () => {
+    expanded.value = withTiming(0, { duration: 400 });
+  };
+
+  const onSend = () => {
+    // todo
+    onShouldSendMessage(message);
+    setMessage('');
+  };
+
+  const onChangeText = (text: string) => {
+    collapseItems();
+    setMessage(text);
+  };
+
+  const expandedButtonStyle = useAnimatedStyle(() => {
+    const opacityInterpolation = interpolate(
+      expanded.value,
+      [0, 1],
+      [1, 0],
+      Extrapolation.CLAMP
+    );
+
+    const widthInterpolation = interpolate(
+      expanded.value,
+      [0, 1],
+      [30, 0],
+      Extrapolation.CLAMP
+    );
+
+    return {
+      opacity: opacityInterpolation,
+      width: widthInterpolation,
+    };
+  });
+
+  const buttonViewStyle = useAnimatedStyle(() => {
+    const widthInterpolation = interpolate(
+      expanded.value,
+      [0, 1],
+      [0, 100],
+      Extrapolation.CLAMP
+    );
+
+    return {
+      opacity: expanded.value,
+      width: widthInterpolation,
+    };
+  });
+
+  return (
+    <BlurView
+      intensity={90}
+      tint='extraLight'
+      style={{ paddingBottom: bottom + 20, paddingTop: 10 }}
+    >
+      <View style={styles.row}>
+        <ATouchableOpacity
+          onPress={expandItems}
+          style={[styles.roundBtn, expandedButtonStyle]}
+        >
+          <Ionicons name='add' size={24} color={Colors.grey} />
+        </ATouchableOpacity>
+
+        <Animated.View style={[styles.buttonView, buttonViewStyle]}>
+          <TouchableOpacity onPress={() => ImagePicker.launchCameraAsync()}>
+            <Ionicons name='camera-outline' size={24} color={Colors.grey} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => ImagePicker.launchImageLibraryAsync()}
+          >
+            <Ionicons name='image-outline' size={24} color={Colors.grey} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => DocumentPicker.getDocumentAsync()}>
+            <Ionicons name='folder-outline' size={24} color={Colors.grey} />
+          </TouchableOpacity>
+        </Animated.View>
+
+        <TextInput
+          autoFocus
+          placeholder='Message'
+          style={styles.messageInput}
+          multiline
+          value={message}
+          onChangeText={onChangeText}
+          onFocus={collapseItems}
+          returnKeyType='done'
+        />
+
+        {message.length > 0 ? (
+          <TouchableOpacity onPress={onSend}>
+            <Ionicons name='arrow-up-circle' size={30} color={Colors.grey} />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity>
+            {/* <Ionicons name='mic' size={30} color={Colors.grey} /> */}
+            <FontAwesome6 name='microphone' size={30} color={Colors.grey} />
+          </TouchableOpacity>
+        )}
+      </View>
+    </BlurView>
+  );
+};
+
+export default MessageInput;
+
+// const ATouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity);
+
+// export type Props = {
+//   onShouldSend: (message: string) => void;
+// };
+
+// const MessageInput = ({ onShouldSend }: Props) => {
+//   const [message, setMessage] = useState('');
+//   const { bottom } = useSafeAreaInsets();
+//   const expanded = useSharedValue(0);
+//   const inputRef = useRef<TextInput>(null);
+
+//   const expandItems = () => {
+//     expanded.value = withTiming(1, { duration: 400 });
+//   };
+
+//   const collapseItems = () => {
+//     expanded.value = withTiming(0, { duration: 400 });
+//   };
+
+//   const expandButtonStyle = useAnimatedStyle(() => {
+//     const opacityInterpolation = interpolate(
+//       expanded.value,
+//       [0, 1],
+//       [1, 0],
+//       Extrapolation.CLAMP
+//     );
+//     const widthInterpolation = interpolate(
+//       expanded.value,
+//       [0, 1],
+//       [30, 0],
+//       Extrapolation.CLAMP
+//     );
+
+//     return {
+//       opacity: opacityInterpolation,
+//       width: widthInterpolation,
+//     };
+//   });
+
+//   const buttonViewStyle = useAnimatedStyle(() => {
+//     const widthInterpolation = interpolate(
+//       expanded.value,
+//       [0, 1],
+//       [0, 100],
+//       Extrapolation.CLAMP
+//     );
+//     return {
+//       width: widthInterpolation,
+//       opacity: expanded.value,
+//     };
+//   });
+
+//   const onChangeText = (text: string) => {
+//     collapseItems();
+//     setMessage(text);
+//   };
+
+//   const onSend = () => {
+//     onShouldSend(message);
+//     setMessage('');
+//   };
+
+//   const onSelectCard = (text: string) => {
+//     onShouldSend(text);
+//   };
+
+//   return (
+//     <BlurView
+//       intensity={90}
+//       tint='extraLight'
+//       style={{ paddingBottom: bottom, paddingTop: 10 }}
+//     >
+//       <View style={styles.row}>
+//         <ATouchableOpacity
+//           onPress={expandItems}
+//           style={[styles.roundBtn, expandButtonStyle]}
+//         >
+//           <Ionicons name='add' size={24} color={Colors.grey} />
+//         </ATouchableOpacity>
+
+//         <Animated.View style={[styles.buttonView, buttonViewStyle]}>
+//           <TouchableOpacity onPress={() => ImagePicker.launchCameraAsync()}>
+//             <Ionicons name='camera-outline' size={24} color={Colors.grey} />
+//           </TouchableOpacity>
+//           <TouchableOpacity
+//             onPress={() => ImagePicker.launchImageLibraryAsync()}
+//           >
+//             <Ionicons name='image-outline' size={24} color={Colors.grey} />
+//           </TouchableOpacity>
+//           <TouchableOpacity onPress={() => DocumentPicker.getDocumentAsync()}>
+//             <Ionicons name='folder-outline' size={24} color={Colors.grey} />
+//           </TouchableOpacity>
+//         </Animated.View>
+
+//         <TextInput
+//           autoFocus
+//           ref={inputRef}
+//           placeholder='Message'
+//           style={styles.messageInput}
+//           onFocus={collapseItems}
+//           onChangeText={onChangeText}
+//           value={message}
+//           multiline
+//         />
+//         {message.length > 0 ? (
+//           <TouchableOpacity onPress={onSend}>
+//             <Ionicons name='arrow-up-circle' size={24} color={Colors.grey} />
+//           </TouchableOpacity>
+//         ) : (
+//           <TouchableOpacity>
+//             <FontAwesome5 name='headphones' size={24} color={Colors.grey} />
+//           </TouchableOpacity>
+//         )}
+//       </View>
+//     </BlurView>
+//   );
+// };
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  messageInput: {
+    flex: 1,
+    marginHorizontal: 10,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 20,
+    padding: 10,
+    borderColor: Colors.greyLight,
+    backgroundColor: Colors.light,
+  },
+  roundBtn: {
+    width: 30,
+    height: 30,
+    borderRadius: 20,
+    backgroundColor: Colors.input,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 12,
+  },
+});
+// export default MessageInput;
