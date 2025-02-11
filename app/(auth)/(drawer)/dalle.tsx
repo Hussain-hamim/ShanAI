b1e37d44e11fbf5942f81fd2dc827daa638fd8ef
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-02-11 11:44:26 +0430

    clean up

--- app/(auth)/(drawer)/dalle.tsx
+++ app/(auth)/(drawer)/dalle.tsx
@@ -2,26 +2,29 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import { Stack } from 'expo-router';
 import { defaultStyles } from '@/constants/Styles';
+import HeaderDropDown from '@/components/HeaderDropDown';
 
 const Dalle = () => {
   return (
     <View style={defaultStyles.pageContainer}>
       <Stack.Screen
-        options={
-          {
-            // headerTitle: () => (
-            //   <HeaderDropDown
-            //     title='Dall-E'
-            //     onSelect={(key) =>   {} }
-            //     items={[
-            //       { key: 'share', title: 'Share GPT', icon: 'square.and.arrow.up' },
-            //       { key: 'details', title: 'See Details', icon: 'info.up' },
-            //       { key: 'Keep', title: 'Keep in Sidebar', icon: 'pin' },
-            //     ]}
-            //   />
-            // ),
-          }
-        }
+        options={{
+          headerTitle: () => (
+            <HeaderDropDown
+              title='Dall-E'
+              onSelect={(key) => {}}
+              items={[
+                {
+                  key: 'share',
+                  title: 'Share GPT',
+                  icon: 'square.and.arrow.up',
+                },
+                { key: 'details', title: 'See Details', icon: 'info.up' },
+                { key: 'Keep', title: 'Keep in Sidebar', icon: 'pin' },
+              ]}
+            />
+          ),
+        }}
       />
 
       <Text>Dalle</Text>
